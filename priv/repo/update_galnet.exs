d09fc5de67448403ab920aad6183162201a70684
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-03-10 12:43:22 -0700

    :memo:

--- priv/repo/update_galnet.exs
+++ priv/repo/update_galnet.exs
@@ -1,3 +1,7 @@
+# Script to manually update the Galnet stories
+#
+# Execute in production with: `heroku run "POOL_SIZE=2 mix run priv/repo/update_galnet.exs"`
+
 Application.ensure_all_started(:inets)
 Application.ensure_all_started(:ssl)
 
