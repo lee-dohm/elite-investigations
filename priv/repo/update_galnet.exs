2e685941d0fcf6d2f357901a08b38737bb2c13ed
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-03-10 08:44:30 -0700

    DRY up the update GalNet code

--- priv/repo/update_galnet.exs
+++ priv/repo/update_galnet.exs
@@ -1,16 +1,6 @@
 Application.ensure_all_started(:inets)
 Application.ensure_all_started(:ssl)
 
-alias EliteInvestigations.Elite
+alias EliteInvestigations.Galnet
 
-feed =
-  'https://elitedangerous-website-backend-production.elitedangerous.com/api/galnet?_format=json'
-
-{:ok, resp} = :httpc.request(:get, {feed, []}, [], body_format: :binary)
-{{_, 200, 'OK'}, _headers, body} = resp
-
-body
-|> Jason.decode!(keys: :atoms)
-|> Enum.each(fn story ->
-  unless Elite.story_exists?(story.nid), do: Elite.create_story(story)
-end)
+Galnet.update()
