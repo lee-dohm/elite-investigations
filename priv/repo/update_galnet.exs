8d55f4fef55c1fc9159aa74afbf0bef5a90b381d
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-03-10 07:50:04 -0700

    Format update GalNet script

--- priv/repo/update_galnet.exs
+++ priv/repo/update_galnet.exs
@@ -6,11 +6,11 @@ alias EliteInvestigations.Elite
 feed =
   'https://elitedangerous-website-backend-production.elitedangerous.com/api/galnet?_format=json'
 
-{:ok, resp} = :httpc.request(:get, {feed, []}, [], [body_format: :binary])
+{:ok, resp} = :httpc.request(:get, {feed, []}, [], body_format: :binary)
 {{_, 200, 'OK'}, _headers, body} = resp
 
 body
 |> Jason.decode!(keys: :atoms)
 |> Enum.each(fn story ->
-     unless Elite.story_exists?(story.nid), do: Elite.create_story(story)
-   end)
+  unless Elite.story_exists?(story.nid), do: Elite.create_story(story)
+end)
