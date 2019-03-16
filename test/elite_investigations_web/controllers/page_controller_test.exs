9ded99291405dd34e865681d6a309e9f1d99216b
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-03-16 07:42:17 -0700

    Improve home page tests

--- test/elite_investigations_web/controllers/page_controller_test.exs
+++ test/elite_investigations_web/controllers/page_controller_test.exs
@@ -1,8 +1,29 @@
 defmodule EliteInvestigationsWeb.PageControllerTest do
   use EliteInvestigationsWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Elite: Investigations"
+  describe "GET /" do
+    setup %{conn: conn} do
+      conn = get(conn, "/")
+      body = html_response(conn, 200)
+
+      {:ok, body: body, conn: conn}
+    end
+
+    test "masthead logo link exists", %{body: body} do
+      masthead_logo = Floki.find(body, "a.masthead-logo")
+
+      assert Floki.attribute(masthead_logo, "href") == ["/"]
+      assert Floki.text(masthead_logo) == "Elite: Investigations"
+    end
+
+    test "subreddit link exists", %{body: body} do
+      subreddit_link = Floki.find(body, "nav.masthead-nav a")
+
+      assert Floki.attribute(subreddit_link, "href") == [
+               "https://www.reddit.com/r/EliteInvestigations"
+             ]
+
+      assert Floki.text(subreddit_link) == "Subreddit"
+    end
   end
 end
